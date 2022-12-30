a836c286fc1481fd4b7e56a5f08be9172644158f
ariellezcano <ariellezcano1995@gmail.com>
2022-12-30 13:27:47 -0300

    ...

--- sistemaSued/src/environments/environment.prod.ts
+++ sistemaSued/src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   URL: 'https://policiadigitalchaco.com.ar/apiSued/api/',
   URLOci: 'https://policiadigital.chaco.gob.ar:9090/api_oci/',
   URLGeolocalizacion: 'https://api.geoapify.com/v1/geocode/search?text=',
-  URLCivil: 'http://10.125.31.74/api_civil/',
+  URLCivil: 'https://policiadigital.chaco.gob.ar:9090/api_civil/',
   //URL: 'http://10.125.31.10:8888/api_oci/',
-  URLRegBus: 'http://10.125.31.74/api_registroUsuario/',
+  URLRegBus: 'https://policiadigital.chaco.gob.ar:9090/api_registroUsuario/',
 };
