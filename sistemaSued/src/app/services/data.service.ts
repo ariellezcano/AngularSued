75e2113b3d21ae4a90ce7aa8b98aff70eda7a50f
ariellezcano <ariellezcano1995@gmail.com>
2023-05-30 12:42:14 -0300

    ...

--- sistemaSued/src/app/services/data.service.ts
+++ sistemaSued/src/app/services/data.service.ts
@@ -1,12 +1,10 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  //private dataArray = new Subject<any[]>();
   dataArray: any[];
 
   constructor() {
