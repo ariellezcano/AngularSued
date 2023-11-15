03a0563448fcd421e4a7fc737ae1f7d58f3fbf77
ariellezcano <ariellezcano1995@gmail.com>
2023-11-15 13:54:13 -0300

    ...

--- sistemaSued/src/environments/environment.prod.ts
+++ sistemaSued/src/environments/environment.prod.ts
@@ -1,11 +1,20 @@
 export const environment = {
   production: true,
-  //URL: 'http://192.168.5.210/apiSued/api/',
+  // real
   URL: 'http://10.125.31.149/api/',
-  //URL: 'https://policiadigitalchaco.com.ar/apiSued/api/',
   URLOci: 'http://10.125.31.23/api_mesaEntrada/',
   URLGeolocalizacion: 'https://api.geoapify.com/v1/geocode/search?text=',
   URLCivil: 'https://policiadigital.chaco.gob.ar:9090/api_civil/',
   //URL: 'http://10.125.31.10:8888/api_oci/',
   URLRegBus: 'https://policiadigital.chaco.gob.ar:9090/api_registroUsuario/',
+
+
+//prueba
+  // URL: 'http://10.125.30.200/apiSued/api/',
+
+  // //URL: 'https://policiadigitalchaco.com.ar/apiSued/api/',
+  // URLOci: 'http://10.125.31.23/api_mesaEntrada/',
+  // URLGeolocalizacion: 'https://api.geoapify.com/v1/geocode/search?text=',
+  // URLCivil: 'http://10.125.30.74/api_civil/',
+  // URLRegBus: 'http://10.125.30.74/api_registroUsuario/',
 };
